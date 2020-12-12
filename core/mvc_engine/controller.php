b0d8b3d6c2dd003f629e88adf34d52d1b113bfe3
jose-mc-b <75644861+jose-mc-b@users.noreply.github.com>
2020-12-11 22:49:58 -0600

    Update controller.php

--- core/mvc_engine/controller.php
+++ core/mvc_engine/controller.php
@@ -10,13 +10,13 @@ abstract class controller {
     public function __construct ($resource='', $arg="", $api=false){
         $this->api = $api;
         $this->apidata ='';
-        $this->model = MVCEngineHelper::get_model ($this); //CORREGIDO
+        $this->model = MVCEngineHelper::get_model ($this); 
         $this->view = MVCEngineHelper::get_view($this);
         
-        if (method_exists ($this, $resource)){  //CORREGIDO
+        if (method_exists ($this, $resource)){  
             //-- llamar la clase con su método correspondiente
             // -- y le manda los argumentos correspondientes en caso de 
-            call_user_func(array($this, $resource), $arg);  //CORREGIDO
+            call_user_func(array($this, $resource), $arg);  
         } 
         else {
             HTTPHelper::go(DEFAULT_VIEW);
