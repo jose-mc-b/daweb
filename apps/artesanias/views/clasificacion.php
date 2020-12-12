885e8777bce90b176976b39cd923b65e4a7d1b3f
jose-mc-b <75644861+jose-mc-b@users.noreply.github.com>
2020-12-11 22:11:05 -0600

    Actualizacion ClasificacionView
    Se añade función agregar y editar.

--- apps/artesanias/views/clasificacion.php
+++ apps/artesanias/views/clasificacion.php
@@ -3,7 +3,10 @@
 class ClasificacionView  {
 
     public function agregar(){
-        echo "Metodo agregar de clase ClasificacionViews";
+         $str = file_get_contents(
+            STATIC_DIR . "html/artesanias/clasificacion_agregar.html"); 
+        $html = Template($str)->render_regex('LISTADO_CLASIFICACION', $list);
+        print Template('Agregar clasificacion')->show($html);
     } 
 
     public function listar($list=array()) {
@@ -12,6 +15,14 @@ class ClasificacionView  {
         $html = Template($str)->render_regex('LISTADO_CLASIFICACION', $list);
         print Template('Listado de clasificacion')->show($html);
     }
+    
+    public function editar($list=[], $clasificacion){
+        $str = file_get_contents(
+            STATIC_DIR . "html/artesanias/clasificacion_editar.html"); 
+        $html = Template($str)->render_regex('LISTADO_CLASIFICACION', $list);
+        $html = Template($html)->render($clasificacion);
+        print Template('Editar clasificacion')->show($html);
+    } 
 
 }
 
